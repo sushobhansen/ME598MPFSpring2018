1e442cd655d98051372c997364a28ff02877a794
sushobhan sen <icecoolsushobhan@gmail.com>
2018-02-09 09:14:22 -0600

    Added a Python plotting script

---
+++ CP1/plotmesh.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Read Data
+x,y,u,v,p = np.loadtxt('velocity.plt',delimiter=',',dtype=np.float,skiprows=1,unpack=True)
+
+#Reshape data from vector to a matrix (mesh)
+N = int(np.sqrt(len(x)))
+x = np.reshape(x,(N,N))
+y = np.reshape(y,(N,N))
+u = np.reshape(u,(N,N))
+v = np.reshape(v,(N,N))
+p = np.reshape(p,(N,N))
+
+#Plot data
+#Plot u
+plt.figure(facecolor='white')
+plt.contourf(x,y,u,levels=np.arange(np.min(u),np.max(u),0.01),cmap=plt.cm.jet)
+plt.colorbar()
+plt.title('u')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.xlim(0,1)
+plt.ylim(0,1)
+plt.savefig('u.jpg')
+
+#Plot v
+plt.figure(facecolor='white')
+plt.contourf(x,y,v,levels=np.arange(np.min(v),np.max(v),0.01),cmap=plt.cm.jet)
+plt.colorbar()
+plt.title('v')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.xlim(0,1)
+plt.ylim(0,1)
+plt.savefig('v.jpg')
+
+#Plot p
+plt.figure(facecolor='white')
+plt.contourf(x,y,p,levels=np.arange(np.min(p),np.max(p),0.5),cmap=plt.cm.jet)
+plt.colorbar()
+plt.title('p')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.xlim(0,1)
+plt.ylim(0,1)
+plt.savefig('p.jpg')
+
+plt.show()
